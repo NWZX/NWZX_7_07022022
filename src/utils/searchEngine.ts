ec592ade4a0720607aae6d04d94a81de1ef64814
NWZX <nwzx@users.noreply.github.com>
2022-03-16 19:21:36 -0400

    Move alternative to branche

--- src/utils/searchEngine.ts
+++ src/utils/searchEngine.ts
@@ -1,7 +1,16 @@
 import { IRecipe } from '../data/Interface';
 
+const AlternativeEvery = <T>(data: T[], predicate: (item: T) => boolean): boolean => {
+    for (let i = 0; i < data.length; i++) {
+        if (!predicate(data[i])) {
+            return false;
+        }
+    }
+    return true;
+};
+
 /**
- * Classic version of SearchEngine
+ * Alternative version of SearchEngine
  * @param data Array of IRecipe
  * @param search Researched strings
  * @param advancedMode Expend research to ustensils and appliance
@@ -9,19 +18,30 @@ import { IRecipe } from '../data/Interface';
  */
 export const SearchEngine = async (data: IRecipe[], search: string, advancedMode = false): Promise<IRecipe[]> => {
     const searchArray = search.toLowerCase().split(' ');
-    return data.filter((recipe) => {
-        return searchArray.every((searchWord) => {
+    const result = new Array(data.length);
+    let j = 0;
+    for (let i = 0; i < data.length; i++) {
+        const isValid = AlternativeEvery(searchArray, (searchWord) => {
             return !(
-                recipe.name.toLowerCase().indexOf(searchWord) === -1 &&
-                recipe.description.toLowerCase().indexOf(searchWord) === -1 &&
-                recipe.ingredients.every(
+                data[i].name.toLowerCase().indexOf(searchWord) === -1 &&
+                data[i].description.toLowerCase().indexOf(searchWord) === -1 &&
+                AlternativeEvery(
+                    data[i].ingredients,
                     (ingredient) => ingredient.ingredient.toLowerCase().indexOf(searchWord) === -1,
                 ) &&
                 (advancedMode
-                    ? recipe.ustensils.every((ustensil) => ustensil.toLowerCase().indexOf(searchWord) === -1)
+                    ? AlternativeEvery(
+                          data[i].ustensils,
+                          (ustensil) => ustensil.toLowerCase().indexOf(searchWord) === -1,
+                      )
                     : true) &&
-                (advancedMode ? recipe.appliance.toLowerCase().indexOf(searchWord) === -1 : true)
+                (advancedMode ? data[i].appliance.toLowerCase().indexOf(searchWord) === -1 : true)
             );
         });
-    });
+        if (isValid) {
+            result[j] = data[i];
+            j++;
+        }
+    }
+    return result.slice(0, j);
 };
